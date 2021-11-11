8302a9e9ef0236800b17d4a58bb5c0be371b725d
Abert Boada <albert.boada@socialpoint.es>
2021-11-11 17:33:37 +0100

    (Capstone15): Indent with spaces

--- capstone-lesson-15/main.go
+++ capstone-lesson-15/main.go
@@ -4,72 +4,72 @@ import "fmt"
 
 type fahrenheit float64
 func (f fahrenheit) celsius() celsius {
-	return celsius((f - 32.0) * 5.0 / 9.0)
+    return celsius((f - 32.0) * 5.0 / 9.0)
 }
 
 type celsius float64
 func (c celsius) fahrenheit() fahrenheit {
-	return fahrenheit((c * 9.0/5.0) + 32.0)
+    return fahrenheit((c * 9.0/5.0) + 32.0)
 }
 
 type converterFn func (value float64) float64
 
 func main() {
-	from := -40.0
-	to := 100.0
-	step := 5.0
+    from := -40.0
+    to := 100.0
+    step := 5.0
 
-	const celsiusHeader = "ºC"
-	const fahrenheitHeader = "ºF"
+    const celsiusHeader = "ºC"
+    const fahrenheitHeader = "ºF"
 
-	drawTable(from, to, step, celsiusHeader, fahrenheitHeader, c2f)
-	drawTable(from, to, step, fahrenheitHeader, celsiusHeader, f2c)
+    drawTable(from, to, step, celsiusHeader, fahrenheitHeader, c2f)
+    drawTable(from, to, step, fahrenheitHeader, celsiusHeader, f2c)
 }
 
 func drawTable(
-	from float64,
-	to float64,
-	step float64,
-	firstColumn string,
-	secondColum string,
-	converter converterFn,
+    from float64,
+    to float64,
+    step float64,
+    firstColumn string,
+    secondColum string,
+    converter converterFn,
 ) {
-	drawHeader(firstColumn, secondColum)
+    drawHeader(firstColumn, secondColum)
 
-	for from <= to {
-		drawRow(from, converter(from))
-		from+=step
-	}
+    for from <= to {
+        drawRow(from, converter(from))
+        from+=step
+    }
 
-	drawFooter()
+    drawFooter()
 }
 
 func c2f(value float64) float64 {
-	c := celsius(value)
+    c := celsius(value)
 
-	return float64(c.fahrenheit())
+    return float64(c.fahrenheit())
 }
 
 func f2c(value float64) float64 {
-	f := fahrenheit(value)
+    f := fahrenheit(value)
 
-	return float64(f.celsius())
+    return float64(f.celsius())
 }
 
 func drawHeader(firstColumn, secondColumn string) {
-	drawTableSeparator()
-	fmt.Printf("| %8s | %8s |\n", firstColumn, secondColumn)
-	drawTableSeparator()
+    drawTableSeparator()
+    fmt.Printf("| %8s | %8s |\n", firstColumn, secondColumn)
+    drawTableSeparator()
 }
 
 func drawRow(value1 float64, value2 float64) {
-	fmt.Printf("| %8.2f | %8.2f |\n", value1, value2)
+    fmt.Printf("| %8.2f | %8.2f |\n", value1, value2)
 }
 
 func drawFooter() {
-	drawTableSeparator()
+    drawTableSeparator()
 }
 
 func drawTableSeparator() {
-	fmt.Println("=======================")
+    fmt.Println("=======================")
 }
